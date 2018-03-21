8a25c81fdc87954dde3c1fec30ac0b6d19acbcdc
Gregory White <greg@gwvt.me>
2018-03-21 07:32:20 -0400

    04 modify sequences/instruments/all.js

--- tests/e2e/sequences/instruments/all.js
+++ tests/e2e/sequences/instruments/all.js
@@ -2,10 +2,12 @@ const guitar = require('./guitar.js');
 const drums = require('./drums.js');
 const bass = require('./bass.js');
 const saxophone = require('./saxophone.js');
+const keyboard = require('./keyboard.js');
 
 module.exports = {
   guitar,
   drums,
   bass,
   saxophone,
+  keyboard,
 };
