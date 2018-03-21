80e42944b57ce164b5cd404d1f4a2ef5fdd977df
Gregory White <greg@gwvt.me>
2018-03-21 01:10:57 -0400

    04 add sequences/instruments/all.js

---
+++ tests/e2e/sequences/instruments/all.js
@@ -0,0 +1,11 @@
+const guitar = require('./guitar.js');
+const drums = require('./drums.js');
+const bass = require('./bass.js');
+const saxophone = require('./saxophone.js');
+
+module.exports = {
+  guitar,
+  drums,
+  bass,
+  saxophone,
+};
