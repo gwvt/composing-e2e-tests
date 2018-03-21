5d3f930c3126da5d3d89e86dfe537bd351a252cb
Gregory White <greg@gwvt.me>
2018-03-21 01:10:14 -0400

    04 add sequences/albums/all.js

---
+++ tests/e2e/sequences/albums/all.js
@@ -0,0 +1,17 @@
+const gaucho = require('./gaucho.js');
+const aja = require('./aja.js');
+const katyLied = require('./katy-lied.js');
+const theRoyalScam = require('./the-royal-scam.js');
+const cantBuyAThrill = require('./cant-buy-a-thrill.js');
+const pretzelLogic = require('./pretzel-logic.js');
+const countdownToEcstasy = require('./countdown-to-ecstasy.js');
+
+module.exports = {
+  gaucho,
+  aja,
+  katyLied,
+  theRoyalScam,
+  cantBuyAThrill,
+  pretzelLogic,
+  countdownToEcstasy,
+};
