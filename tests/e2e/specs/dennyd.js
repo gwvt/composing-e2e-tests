f62f83fc2298da29a8dfd5c2ee608e30891789c7
Gregory White <greg@gwvt.me>
2018-03-21 07:08:43 -0400

    03 modify specs/dennyd.js

--- tests/e2e/specs/dennyd.js
+++ tests/e2e/specs/dennyd.js
@@ -1,7 +1,7 @@
 const guitar = require('../sequences/instruments/guitar.js');
 const aja = require('../sequences/albums/aja.js');
-const gaucho = require('../sequences/albums/gaucho.js');
 const katyLied = require('../sequences/albums/katy-lied.js');
+const pretzelLogic = require('../sequences/albums/pretzel-logic.js');
 const theRoyalScam = require('../sequences/albums/the-royal-scam.js');
 const cantBuyAThrill = require('../sequences/albums/the-royal-scam.js');
 const countdownToEcstasy = require('../sequences/albums/countdown-to-ecstasy.js');
@@ -17,8 +17,8 @@ module.exports = {
   },
   'and thinks about guitar': guitar.pages,
   'and remembers Aja': aja.pages,
-  'and remembers Gaucho': gaucho.pages,
   'and remembers Katy Lied': katyLied.pages,
+  'and remembers Pretzel Logic': pretzelLogic.pages,
   'and remembers The Royal Scam': theRoyalScam.pages,
   'and remembers Can\'t Buy a Thrill': cantBuyAThrill.pages,
   'and remembers Countdown to Ecstasy': countdownToEcstasy.pages,
