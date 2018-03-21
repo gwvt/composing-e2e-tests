4e327ce9942a8a8ebb08d587e94ff3c96cdf513e
Gregory White <greg@gwvt.me>
2018-03-21 01:02:26 -0400

    03 modify specs/larryc.js, dennyd.js; add toms.js

--- tests/e2e/specs/dennyd.js
+++ tests/e2e/specs/dennyd.js
@@ -1,16 +1,27 @@
 const guitar = require('../sequences/instruments/guitar.js');
 const aja = require('../sequences/albums/aja.js');
+const gaucho = require('../sequences/albums/gaucho.js');
+const katyLied = require('../sequences/albums/katy-lied.js');
+const theRoyalScam = require('../sequences/albums/the-royal-scam.js');
+const cantBuyAThrill = require('../sequences/albums/the-royal-scam.js');
+const countdownToEcstasy = require('../sequences/albums/countdown-to-ecstasy.js');
+const helpers = require('../helpers.js');
+
+const musician = {
+  code: 'dennyd',
+};
 
 module.exports = {
   'Denny Dias opens the app': (browser) => {
-    browser
-      .url(process.env.VUE_DEV_SERVER_URL)
-      .waitForElementVisible('#select-musician', 5000)
-      .click('#select-dennyd')
-      .click('#next-page');
+    (helpers.selectMusician.bind(null, browser, process, musician))();
   },
   'and thinks about guitar': guitar.pages,
   'and remembers Aja': aja.pages,
+  'and remembers Gaucho': gaucho.pages,
+  'and remembers Katy Lied': katyLied.pages,
+  'and remembers The Royal Scam': theRoyalScam.pages,
+  'and remembers Can\'t Buy a Thrill': cantBuyAThrill.pages,
+  'and remembers Countdown to Ecstasy': countdownToEcstasy.pages,
   'and leaves the app': (browser) => {
     browser
       .end();
--- tests/e2e/specs/toms.js
+++ tests/e2e/specs/toms.js
@@ -1,23 +1,19 @@
-const guitar = require('../sequences/instruments/guitar.js');
+const saxophone = require('../sequences/instruments/saxophone.js');
 const aja = require('../sequences/albums/aja.js');
 const gaucho = require('../sequences/albums/gaucho.js');
-const katyLied = require('../sequences/albums/katy-lied.js');
-const theRoyalScam = require('../sequences/albums/the-royal-scam.js');
 const helpers = require('../helpers.js');
 
 const musician = {
-  code: 'larryc',
+  code: 'toms',
 };
 
 module.exports = {
-  'Larry Carlton opens the app': (browser) => {
+  'Tom Scott opens the app': (browser) => {
     (helpers.selectMusician.bind(null, browser, process, musician))();
   },
-  'and thinks about guitar': guitar.pages,
+  'and thinks about saxophone': saxophone.pages,
   'and remembers Aja': aja.pages,
   'and remembers Gaucho': gaucho.pages,
-  'and remembers Katy Lied': katyLied.pages,
-  'and remembers The Royal Scam': theRoyalScam.pages,
   'and leaves the app': (browser) => {
     browser
       .end();
