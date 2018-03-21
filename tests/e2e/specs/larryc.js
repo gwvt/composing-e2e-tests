9d67cdd94fa14b637d3c03c0385a813364771286
Gregory White <greg@gwvt.me>
2018-03-21 01:18:28 -0400

    05 modify specs/*

--- tests/e2e/specs/larryc.js
+++ tests/e2e/specs/larryc.js
@@ -1,31 +1,5 @@
-const camel = require('to-camel-case');
+const musicianTests = require('../musician-tests.js');
 
-const instruments = require('../sequences/instruments/all.js');
-const albums = require('../sequences/albums/all.js');
-const helpers = require('../helpers.js');
-
-const data = require('../data-api/data.js');
-const queries = require('../data-api/queries.js');
-
-const musicianCode = 'larryc';
-
-const musician = queries.musicianFromCode(data.musicians, musicianCode);
-musician.albumsPlayedOn = queries.albumsPlayedOn(data.albums, musician);
-
-const tests = {};
-
-tests[`${musician.name} opens the app`] = function test(browser) {
-  (helpers.selectMusician.bind(this, browser, process, musician))();
-};
-
-tests[`and thinks about ${musician.instruments.main}`] = instruments[musician.instruments.main].pages;
-
-musician.albumsPlayedOn.forEach((a) => {
-  tests[`and remembers ${a.title}`] = albums[camel(a.code)].pages;
-});
-
-tests['and leaves the app'] = function test(browser) {
-  browser.end();
-};
+const tests = musicianTests.compose('larryc');
 
 module.exports = tests;
